97a7807bee68c2c7a4e9aed0fae4809b54ca9f20
diegomayorga <diegoamayorgat@gmail.com>
2025-01-23 20:41:24 -0500

    Quiz app finished. Summary Component of correct, wrong and skipped answers was created and the bugs were fixed

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -7,10 +7,12 @@ export default function Answers({
   onSelect,
 }) {
   const shuffledAnswers = useRef();
+
   if (!shuffledAnswers.current) {
     shuffledAnswers.current = [...answers];
     shuffledAnswers.current.sort(() => Math.random() - 0.5);
   }
+  
   return (
     <ul id="answers">
       {shuffledAnswers.current.map((answer) => {
